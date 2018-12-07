f45ce3ca34afc35116629ec4f17b989e8576ca71
H_S <513126235@qq.com>
2018-12-07 18:56:51 +0800

    修改错误配置

--- config/index.js
+++ config/index.js
@@ -24,38 +24,36 @@ module.exports = {
         pathRewrite: {
           '^/openapi/gank': '/'
         }
-      },
+      }
+    },
+    // Various Dev Server settings
+    host: '192.168.1.103', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-      // Various Dev Server settings
-      host: 'localhost', // can be overwritten by process.env.HOST
-      port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-      autoOpenBrowser: false,
-      errorOverlay: true,
-      notifyOnErrors: true,
-      poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    // Use Eslint Loader?
+    // If true, your code will be linted during bundling and
+    // linting errors and warnings will be shown in the console.
+    useEslint: true,
+    // If true, eslint errors and warnings will also be shown in the error overlay
+    // in the browser.
+    showEslintErrorsInOverlay: false,
 
-      // Use Eslint Loader?
-      // If true, your code will be linted during bundling and
-      // linting errors and warnings will be shown in the console.
-      useEslint: true,
-      // If true, eslint errors and warnings will also be shown in the error overlay
-      // in the browser.
-      showEslintErrorsInOverlay: false,
+    /**
+     * Source Maps
+     */
 
-      /**
-       * Source Maps
-       */
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'cheap-module-eval-source-map',
 
-      // https://webpack.js.org/configuration/devtool/#development
-      devtool: 'cheap-module-eval-source-map',
-
-      // If you have problems debugging vue-files in devtools,
-      // set this to false - it *may* help
-      // https://vue-loader.vuejs.org/en/options.html#cachebusting
-      cacheBusting: true,
-
-      cssSourceMap: true
-    }
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: true,
+    cssSourceMap: true
   },
 
   build: {
